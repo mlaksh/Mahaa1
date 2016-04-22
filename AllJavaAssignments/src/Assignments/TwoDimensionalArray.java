3fe9ccfc50bfed294d44180c8a1e7735b2a29244
lakshmi <lakshmi@maha>
2016-04-22 06:29:15 -0700

    .

--- AllJavaAssignments/src/Assignments/TwoDimensionalArray.java
+++ AllJavaAssignments/src/Assignments/TwoDimensionalArray.java
@@ -3,7 +3,11 @@ package Assignments;
 public class TwoDimensionalArray {
 
 	public static void main(String[] args) {
-		int[][] details = {{30,5},{20,4},{50,2}};
+		int[][] details = { { 30, 5 }, { 20, 4 }, { 50, 2 } };
+		for (int[] a : details) {
+
+			System.out.println(a[0] + " == " + a[1]);
+		}
 	}
 
 }
